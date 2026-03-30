3143221960d99fec702c17aa31a517195d1520e5
Arkhan <49446701+DamianQualshy@users.noreply.github.com>
2026-03-31 00:27:29 +0200

    Update RevivedHelpers.d
    WIP

--- CONTENT/Story/RevivedHelpers.d
+++ CONTENT/Story/RevivedHelpers.d
@@ -4,3 +4,23 @@ func void REV_SetObjectRoutine_Fire (var STRING mobName)
 	Wld_SetObjectRoutine (20,00, mobName, 1);
 	Wld_SetObjectRoutine (05,00, mobName, 0);
 };
+
+
+func string REV_BuildTradeString (var int itemInstance)
+{
+	var string concatText;
+	var C_Item itm; itm = Hlp_GetItem(itemInstance);
+	
+	concatText = itm.name;
+	concatText = ConcatStrings (concatText, 		" (Weapon Prot. ");
+	concatText = ConcatStrings (concatText, IntToString(itm.protection[PROT_EDGE]));
+	concatText = ConcatStrings (concatText, 		", Arrow Prot. ");
+	concatText = ConcatStrings (concatText, IntToString(itm.protection[PROT_POINT]));
+	concatText = ConcatStrings (concatText, 		", Magic Prot. ");
+	concatText = ConcatStrings (concatText, IntToString(itm.protection[PROT_MAGIC]));
+	concatText = ConcatStrings (concatText, 		"): ");
+	concatText = ConcatStrings (concatText, IntToString(itm.value));
+	concatText = ConcatStrings (concatText, 		" gold.");
+	
+	return concatText;
+};
