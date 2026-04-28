a3831845945414d65fd745a77a66cbdaae94f1b1
Arkhan <49446701+DamianQualshy@users.noreply.github.com>
2026-04-28 21:17:53 +0200

    Fixed Old Camp's castle main gate not triggering the Orc Attack

--- CONTENT/Story/Events/EVT_OC_MainGate.d
+++ CONTENT/Story/Events/EVT_OC_MainGate.d
@@ -6,57 +6,57 @@ func void EVT_OC_MAINGATE_FUNC_S1 ()
 {
 	if (MIS_OCGateOpen == FALSE)
 	{
-	Wld_InsertNpc		(OrcElite_Roam,"OC_TO_GUARD");
-	Wld_InsertNpc		(OrcWarrior_Roam,"OC_TO_GUARD");
-	Wld_InsertNpc		(OrcElite_Roam,"OC_TO_GUARD");
-	Wld_InsertNpc		(OrcWarrior_Roam,"OC_TO_GUARD");
-	Wld_InsertNpc		(OrcShaman_Sit,"OC_TO_GUARD");
-	Wld_InsertNpc		(OrcElite_Roam,"OC_TO_GUARD");
-	Wld_InsertNpc		(Warg,"OC_GUARD_ENTRANCE");
-	Wld_InsertNpc		(OrcElite_Roam,"OC_GUARD_ENTRANCE");
-	Wld_InsertNpc		(Warg,"OC_GUARD_ENTRANCE");
-	Wld_InsertNpc		(OrcElite_Roam,"OC_GUARD_ENTRANCE");
-	Wld_InsertNpc		(OrcWarrior_Roam,"OC_TRAIN_03");
-	Wld_InsertNpc		(OrcElite_Roam,"OC_TRAIN_03");
-	Wld_InsertNpc		(OrcWarrior_Roam,"OC_TRAIN_03");
-	Wld_InsertNpc		(OrcElite_Roam,"OC_TRAIN_03");
-	Wld_InsertNpc		(OrcWarrior_Roam,"OC_TRAIN_04");
-	Wld_InsertNpc		(OrcElite_Roam,"OC_TRAIN_04");
-	Wld_InsertNpc		(OrcWarrior_Roam,"OC_CAMPFIRE_OUT_01");
-	Wld_InsertNpc		(Warg,"OC_CAMPFIRE_OUT_01");
-	Wld_InsertNpc		(Warg,"OC_CAMPFIRE_OUT_01");
-	Wld_InsertNpc		(OrcElite_Roam,"OC_CAMPFIRE_OUT_01");
-	Wld_InsertNpc		(OrcWarrior_Roam,"OC_CENTER_02");
-	Wld_InsertNpc		(OrcElite_Roam,"OC_CENTER_02");
-	Wld_InsertNpc		(OrcWarrior_Roam,"OC_CENTER_03");
-	Wld_InsertNpc		(OrcElite_Roam,"OC_CENTER_03");
-	Wld_InsertNpc		(OrcWarrior_Roam,"OC_KITCHEN_ENTRANCE");
-	Wld_InsertNpc		(OrcElite_Roam,"OC_KITCHEN_ENTRANCE");
-	Wld_InsertNpc		(OrcShaman_Sit,"OC_KITCHEN_ENTRANCE");
-	Wld_InsertNpc		(OrcElite_Roam,"OC_KITCHEN_ENTRANCE");
-	Wld_InsertNpc		(OrcWarrior_Roam,"OC_EBR_HALL_THRONE");
-	Wld_InsertNpc		(OrcShaman_Sit,"OC_EBR_HALL_THRONE");
-	Wld_InsertNpc		(Warg,"OC_EBR_HALL_THRONE");
-	
-	//Joly: Palisadenwachen killen, damit sie, nach OrcSturm, nicht wieder rumzumpelderweise versuchen, auf ihren Posten zu kommen.
-	
-	B_KillNpc     	(GRD_4143_HaupttorWache);
-	B_KillNpc     	(GRD_4145_Waffenknecht);
-	B_KillNpc     	(GRD_4146_Waffenknecht);
-	B_KillNpc     	(GRD_4147_Waffenknecht);
-	B_KillNpc     	(GRD_4101_Waffenknecht);
-	B_KillNpc     	(GRD_4102_Waffenknecht);
-	B_KillNpc     	(GRD_4103_Waffenknecht);
-	B_KillNpc     	(GRD_4104_Waffenknecht);
-	B_KillNpc     	(GRD_4105_Waffenknecht);
-	B_KillNpc     	(GRD_4108_Engor);
+		Wld_InsertNpc		(OrcElite_Roam,"OCC_CENTER_1");
+		Wld_InsertNpc		(OrcWarrior_Roam,"OCC_CENTER_1");
+		Wld_InsertNpc		(OrcElite_Roam,"OCC_CENTER_1");
+		Wld_InsertNpc		(OrcWarrior_Roam,"OCC_CENTER_1");
+		Wld_InsertNpc		(OrcShaman_Sit,"OCC_CENTER_1");
+		Wld_InsertNpc		(OrcElite_Roam,"OCC_CENTER_1");
+		Wld_InsertNpc		(Warg,"OCC_WALLCRACK");
+		Wld_InsertNpc		(OrcElite_Roam,"OCC_WALLCRACK");
+		Wld_InsertNpc		(Warg,"OCC_WALLCRACK");
+		Wld_InsertNpc		(OrcElite_Roam,"OCC_WALLCRACK");
+		Wld_InsertNpc		(OrcWarrior_Roam,"OCC_GATE_INSIDE");
+		Wld_InsertNpc		(OrcElite_Roam,"OCC_GATE_INSIDE");
+		Wld_InsertNpc		(OrcWarrior_Roam,"OCC_GATE_INSIDE");
+		Wld_InsertNpc		(OrcElite_Roam,"OCC_GATE_INSIDE");
+		Wld_InsertNpc		(OrcWarrior_Roam,"OCC_CONVOY_01");
+		Wld_InsertNpc		(OrcElite_Roam,"OCC_CONVOY_01");
+		Wld_InsertNpc		(Warg,"OCC_CONVOY_01");
+		Wld_InsertNpc		(OrcWarrior_Roam,"OCC_CELLAR_HALL");
+		Wld_InsertNpc		(OrcElite_Roam,"OCC_CELLAR_HALL");
+		Wld_InsertNpc		(OrcWarrior_Roam,"OCC_BARONS_HALLWAY_FRONT");
+		Wld_InsertNpc		(Warg,"OCC_BARONS_HALLWAY_FRONT");
+		Wld_InsertNpc		(Warg,"OCC_BARONS_HALLWAY_FRONT");
+		Wld_InsertNpc		(OrcElite_Roam,"OCC_BARONS_HALLWAY_FRONT");
+		Wld_InsertNpc		(OrcShaman_Sit,"OCC_BARONS_HALLWAY_FRONT");
+		Wld_InsertNpc		(OrcWarrior_Roam,"OCC_BARONS_UPSTAIRS_PROMENADE_LEFT_DOOR");
+		Wld_InsertNpc		(OrcElite_Roam,"OCC_BARONS_UPSTAIRS_PROMENADE_LEFT_DOOR");
+		Wld_InsertNpc		(OrcWarrior_Roam,"OCC_BARONS_UPSTAIRS_PROMENADE_LEFT_DOOR");
+		Wld_InsertNpc		(OrcWarrior_Roam,"OCC_BARONS_GREATHALL_CENTER_LEFT");
+		Wld_InsertNpc		(OrcElite_Roam,"OCC_BARONS_GREATHALL_CENTER_LEFT");
+		Wld_InsertNpc		(OrcShaman_Sit,"OCC_BARONS_GREATHALL_CENTER_LEFT");
+		Wld_InsertNpc		(OrcElite_Roam,"OCC_BARONS_GREATHALL_CENTER_LEFT");
+		
+		//Joly: Palisadenwachen killen, damit sie, nach OrcSturm, nicht wieder rumzumpelderweise versuchen, auf ihren Posten zu kommen.
+		
+		B_KillNpc     	(GRD_4143_HaupttorWache);
+		B_KillNpc     	(GRD_4145_Waffenknecht);
+		B_KillNpc     	(GRD_4146_Waffenknecht);
+		B_KillNpc     	(GRD_4147_Waffenknecht);
+		B_KillNpc     	(GRD_4101_Waffenknecht);
+		B_KillNpc     	(GRD_4102_Waffenknecht);
+		B_KillNpc     	(GRD_4103_Waffenknecht);
+		B_KillNpc     	(GRD_4104_Waffenknecht);
+		B_KillNpc     	(GRD_4105_Waffenknecht);
+		B_KillNpc     	(GRD_4108_Engor);
 
-	Garond.flags = 0;
- 	Brutus.flags = NPC_FLAG_IMMORTAL;
+		Garond.flags = 0;
+		Brutus.flags = NPC_FLAG_IMMORTAL;
 
-	PlayVideo ("ORCATTACK.BIK");  
+		PlayVideo ("ORCATTACK.BIK");  
 
-	MIS_OCGateOpen = TRUE;
+		MIS_OCGateOpen = TRUE;
 	};
 };
 
